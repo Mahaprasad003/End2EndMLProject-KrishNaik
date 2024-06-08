16a86a8e02a4167354de1a2598ebc98f8ebfb434
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2024-06-08 11:56:52 +0530

    finished model_trainer.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 #DataIngestionConfig class is basically getting the file paths and storing it
 @dataclass
 class DataIngestionConfig:
@@ -78,5 +81,8 @@ if __name__ == '__main__':
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
     
